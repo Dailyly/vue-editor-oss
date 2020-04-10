6923bb76e7b84ea355eb16b09a7a5e3a465c5760
Dailyly <daily1738451756@163.com>
2020-04-10 16:46:36 +0800

    webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src/main.js'), // 项目中总入口文件，
@@ -12,7 +13,19 @@ module.exports = {
         // 使用vue-loader解析.vue文件
         test: /\.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        // 要加上style-loader才能正确解析.vue文件里的<style>标签的内容
+        use: ['style-loader', 'css-loader']
       }
     ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin()
+  ],
+  devServer: {
+    port: 8080
   }
 }
